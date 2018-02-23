d894c81b760322649e43bc3dbabc3fb7b283d35c
vaneshita <144995@unsaac.edu.pe>
2018-02-23 09:33:30 -0500

    actualizando js

--- public/js/app.js
+++ public/js/app.js
@@ -131,6 +131,25 @@ window.addEventListener('load', function() {
       });
     });
   }
+  // alert
+  $('#btns').on('click', function() {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this imaginary file!',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal('Poof! Your imaginary file has been deleted!', {
+            icon: 'success',
+          });
+        } else {
+          swal('Your imaginary file is safe!');
+        }
+      });
+  });
 });
 
 // Funcion de mayor a menos
