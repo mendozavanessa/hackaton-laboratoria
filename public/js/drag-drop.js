1f431b742967b4b72a0ab4b837b18428159b301d
MelynaPernia <mebipeos@outlook.com>
2018-02-23 12:56:02 -0500

    Añadiendo estilos

--- public/js/drag-drop.js
+++ public/js/drag-drop.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function() {
   document.addEventListener('dragstart', drag);
   document.addEventListener('dragover', permitirDrop);
   document.addEventListener('drop', drop);
+  var boxMarco = document.querySelectorAll('.dropheigth');
   
   function drag(event) {
     console.log(event);
@@ -10,16 +11,20 @@ window.addEventListener('load', function() {
   
   function permitirDrop(event) {
     event.preventDefault();
+
   }
   
   function drop(event) {
     event.preventDefault();
-    console.log(event.target);
+    // console.log(event.target);
+
     if (event.target.dataset.box === 'marco') {
       let idName = event.dataTransfer.getData('text');
-      console.log(idName);
+      // console.log(idName);
       event.target.appendChild(document.getElementById(idName));
     }
   }
+
+  
 });
     
